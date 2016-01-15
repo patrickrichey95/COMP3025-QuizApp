857dc0f0bf7259e066c864c306ff46d5d56d2ce4
student <student@students-mac.student.georgianc.on.ca>
2016-01-15 11:04:13 -0500

    Updated one file

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //initial commit
+        print("The View Did Load")
     }
     
 }
